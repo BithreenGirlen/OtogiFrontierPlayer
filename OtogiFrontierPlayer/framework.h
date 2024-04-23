b112d11307df6813f8667ecdd1adaa29d9a24e90
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-04-23 14:10:25 +0900

    Delete default files

--- OtogiFrontierPlayer/framework.h
+++ OtogiFrontierPlayer/framework.h
@@ -1,11 +1,6 @@
-﻿// header.h : 標準のシステム インクルード ファイルのインクルード ファイル、
-// またはプロジェクト専用のインクルード ファイル
-//
+﻿#pragma once
 
-#pragma once
-
-#include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#include <SDKDDKVer.h>
+#define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
-// Windows ヘッダー ファイル
 #include <windows.h>
