252f79f9ac16eaae8a6398f0aa208b1d36e5ecc8
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-04-23 13:04:31 +0900

    Add icon

--- OtogiFrontierPlayer/resource.h
+++ OtogiFrontierPlayer/resource.h
@@ -3,6 +3,7 @@
 // OtogiFrontierPlayer.rc で使用
 //
 #define IDC_MYICON                      2
+#define IDI_ICON_APP                    129
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -10,7 +11,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
