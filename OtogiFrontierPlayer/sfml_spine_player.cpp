47e8928bd5581baa4993eb964242b925eb7646b7
BithreenGirlen <152838289+BithreenGirlen@users.noreply.github.com>
2024-09-03 20:25:05 +0530

    Read offset values from attachment.
    Compute centroid for mesh attachment.
    Add a switch to enable initial offset.

--- OtogiFrontierPlayer/sfml_spine_player.cpp
+++ OtogiFrontierPlayer/sfml_spine_player.cpp
@@ -86,11 +86,7 @@ bool CSfmlSpinePlayer::SetSpine(const std::vector<std::string>& atlasPaths, cons
 		m_skeletonData.emplace_back(skeletonData);
 	}
 
-	if (m_skeletonData.empty())return false;
-
-	m_fMaxWidth = m_skeletonData.at(0)->width;
-	m_fMaxHeight = m_skeletonData.at(0)->height;
-
+	WorkOutDefualtSize();
 	WorkOutDefaultScale();
 
 	return SetupDrawer();
@@ -109,7 +105,7 @@ int CSfmlSpinePlayer::Display()
 	bool bSpeedHavingChanged = false;
 
 	int iRet = 0;
-	m_window = std::make_unique< sf::RenderWindow>(sf::VideoMode(static_cast<unsigned int>(m_fMaxWidth), static_cast<unsigned int>(m_fMaxHeight)), "Otogi spine player", sf::Style::None);
+	m_window = std::make_unique< sf::RenderWindow>(sf::VideoMode(static_cast<unsigned int>(m_fBaseSize.x), static_cast<unsigned int>(m_fBaseSize.y)), "Otogi spine player", sf::Style::None);
 	m_window->setPosition(sf::Vector2i(0, 0));
 	m_window->setFramerateLimit(0);
 	ResetScale();
@@ -159,7 +155,7 @@ int CSfmlSpinePlayer::Display()
 
 					if (iX == 0 && iY == 0)
 					{
-						ShiftScene();
+						ShiftAnimation();
 					}
 					else
 					{
@@ -219,6 +215,10 @@ int CSfmlSpinePlayer::Display()
 			case sf::Event::KeyReleased:
 				switch (event.key.code)
 				{
+				case sf::Keyboard::Key::D:
+					m_bFDefaultOffSetEnabled ^= true;
+					ResetScale();
+					break;
 				case sf::Keyboard::Key::S:
 					for (size_t i = 0; i < m_drawables.size(); ++i)
 					{
@@ -278,7 +278,7 @@ int CSfmlSpinePlayer::Display()
 bool CSfmlSpinePlayer::SetupDrawer()
 {
 	/*
-	 * The spine outline shader, an extension by Unity, cannot be brought forth by SFML. 
+	 * Spine outline shader, an extension by Unity, cannot be brought forth by SFML. 
 	 * Some scene gets closer to natural with premultiplied alpha disabled, some with enabled.
 	 */
 	const std::vector<std::string> pmaSelectiveList
@@ -290,8 +290,8 @@ bool CSfmlSpinePlayer::SetupDrawer()
 
 		CSfmlSpineDrawableC* drawable = m_drawables.at(i).get();
 		drawable->timeScale = 1.0f;
-		drawable->skeleton->x = m_fMaxWidth / 2;
-		drawable->skeleton->y = m_fMaxHeight / 2;
+		drawable->skeleton->x = m_fBaseSize.x / 2;
+		drawable->skeleton->y = m_fBaseSize.y / 2;
 		spSkeleton_setToSetupPose(drawable->skeleton);
 		spSkeleton_updateWorldTransform(drawable->skeleton);
 
@@ -316,13 +316,139 @@ bool CSfmlSpinePlayer::SetupDrawer()
 
 	return m_animationNames.size() > 0;
 }
-/*標準尺度算出*/
-void CSfmlSpinePlayer::WorkOutDefaultScale()
+/*標準寸法算出*/
+void CSfmlSpinePlayer::WorkOutDefualtSize()
 {
 	if (m_skeletonData.empty())return;
 
-	unsigned int uiSkeletonWidth = static_cast<unsigned int>(m_skeletonData.at(0)->width);
-	unsigned int uiSkeletonHeight = static_cast<unsigned int>(m_skeletonData.at(0)->height);
+	float fMaxSize = 0.f;
+	const auto CompareDimention = [this, &fMaxSize](float fWidth, float fHeight)
+		-> bool
+		{
+			if (fWidth > 0.f && fHeight > 0.f && fWidth * fHeight > fMaxSize)
+			{
+				m_fBaseSize.x = fWidth;
+				m_fBaseSize.y = fHeight;
+				fMaxSize = fWidth * fHeight;
+				return true;
+			}
+
+			return false;
+		};
+
+	for (const auto& pSkeletonData : m_skeletonData)
+	{
+		const char* attachmentName = spSkin_getAttachmentName(pSkeletonData->defaultSkin, 0, 0);
+		if (attachmentName == nullptr)continue;
+
+		spAttachment* pAttachment = spSkin_getAttachment(pSkeletonData->defaultSkin, 0, attachmentName);
+		if (pAttachment == nullptr)continue;
+
+		/*Calculation based on the discussion of forum thread 2998 and 7662*/
+		if (pAttachment->type == SP_ATTACHMENT_REGION)
+		{
+			spRegionAttachment* pRegionAttachment = (spRegionAttachment*)pAttachment;
+
+			bool bRet = CompareDimention(pRegionAttachment->width * pRegionAttachment->scaleX, pRegionAttachment->height * pRegionAttachment->scaleY);
+			if (bRet)
+			{
+				m_fDefaultOffset.x = pRegionAttachment->x * 2.f;
+				m_fDefaultOffset.y = -pRegionAttachment->y * 2.f;
+
+				if (pRegionAttachment->x != 0 || pRegionAttachment->y != 0)
+				{
+					spSlotData* pSlotData = spSkeletonData_findSlot(pSkeletonData, attachmentName);
+					if (pSlotData == nullptr)continue;
+
+					for (int i = 0; i < pSkeletonData->bonesCount; ++i)
+					{
+						if (strcmp(pSlotData->boneData->name, pSkeletonData->bones[i]->name) == 0)
+						{
+							if (pRegionAttachment->y * pSkeletonData->bones[i]->y <= 0)
+							{
+								m_fDefaultOffset.y -= pSkeletonData->bones[i]->y * 2.f;
+							}
+							break;
+						}
+					}
+				}
+			}
+		}
+		else if (pAttachment->type == SP_ATTACHMENT_MESH)
+		{
+			spMeshAttachment* pMeshAttachment = (spMeshAttachment*)pAttachment;
+
+			spSlotData* pSlotData = spSkeletonData_findSlot(pSkeletonData, attachmentName);
+			
+			float fScaleX = pSlotData != nullptr ? pSlotData->boneData->scaleX : 1.f;
+			float fScaleY = pSlotData != nullptr ? pSlotData->boneData->scaleY : 1.f;
+
+			bool bRet = CompareDimention(pMeshAttachment->width * fScaleX, pMeshAttachment->height * fScaleY);
+			if (bRet && pMeshAttachment->super.verticesCount >= pMeshAttachment->hullLength * 2)
+			{
+				const auto WorkoutCentroid = [&pMeshAttachment]()
+					-> sf::Vector2f
+					{
+						sf::Vector2f fCentroid{};
+						float fFilled = 0.f;
+
+						sf::Vector2f fFore
+						{
+							pMeshAttachment->super.vertices[2 * (pMeshAttachment->hullLength - 1)],
+							pMeshAttachment->super.vertices[2 * (pMeshAttachment->hullLength - 1) + 1]
+						};
+						for (int i = 0; i < pMeshAttachment->hullLength; ++i)
+						{
+							sf::Vector2f fNext
+							{
+								pMeshAttachment->super.vertices[2 * i],
+								pMeshAttachment->super.vertices[2 * i + 1]
+							};
+							float fArea = fFore.x * fNext.y - fFore.y * fNext.x;
+							fCentroid.x += (fFore.x + fNext.x) * fArea;
+							fCentroid.y += (fFore.y + fNext.y) * fArea;
+							fFilled += fArea;
+							fFore = fNext;
+						}
+						fCentroid.x /= (6.f * fFilled * 0.5f);
+						fCentroid.y /= (6.f * fFilled * 0.5f);
+
+						return fCentroid;
+					};
+
+				sf::Vector2f fCentroid = WorkoutCentroid();
+
+				m_fDefaultOffset.x = fCentroid.x * 2.f;
+				m_fDefaultOffset.y = -fCentroid.y * 2.f;
+
+				if (fCentroid.x != 0 || fCentroid.y != 0)
+				{
+					spSlotData* pSlotData = spSkeletonData_findSlot(pSkeletonData, attachmentName);
+					if (pSlotData == nullptr)continue;
+
+					for (int i = 0; i < pSkeletonData->bonesCount; ++i)
+					{
+						if (strcmp(pSlotData->boneData->name, pSkeletonData->bones[i]->name) == 0)
+						{
+							m_fDefaultOffset.y -= pSkeletonData->bones[i]->y * 2.f;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (const auto& pSkeletonData : m_skeletonData)
+	{
+		CompareDimention(pSkeletonData->width, pSkeletonData->height);
+	}
+}
+/*標準尺度算出*/
+void CSfmlSpinePlayer::WorkOutDefaultScale()
+{
+	unsigned int uiSkeletonWidth = static_cast<unsigned int>(m_fBaseSize.x);
+	unsigned int uiSkeletonHeight = static_cast<unsigned int>(m_fBaseSize.y);
 
 	unsigned int uiDesktopWidth = sf::VideoMode::getDesktopMode().width;
 	unsigned int uiDesktopHeight = sf::VideoMode::getDesktopMode().height;
@@ -331,15 +457,15 @@ void CSfmlSpinePlayer::WorkOutDefaultScale()
 	{
 		if (uiDesktopWidth > uiDesktopHeight)
 		{
-			m_fDefaultWindowScale = static_cast<float>(uiDesktopHeight) / uiSkeletonHeight;
+			m_fDefaultScale = static_cast<float>(uiDesktopHeight) / uiSkeletonHeight;
 			m_fThresholdScale = static_cast<float>(uiDesktopWidth) / uiSkeletonWidth;
 		}
 		else
 		{
-			m_fDefaultWindowScale = static_cast<float>(uiDesktopWidth) / uiSkeletonWidth;
+			m_fDefaultScale = static_cast<float>(uiDesktopWidth) / uiSkeletonWidth;
 			m_fThresholdScale = static_cast<float>(uiDesktopHeight) / uiSkeletonHeight;
 		}
-		m_fSkeletonScale = m_fDefaultWindowScale;
+		m_fSkeletonScale = m_fDefaultScale;
 	}
 }
 /*尺度設定*/
@@ -364,8 +490,8 @@ void CSfmlSpinePlayer::RescaleTime()
 void CSfmlSpinePlayer::ResetScale()
 {
 	m_fTimeScale = 1.0f;
-	m_fSkeletonScale = m_fDefaultWindowScale;
-	m_iOffset = sf::Vector2i{};
+	m_fSkeletonScale = m_fDefaultScale;
+	m_fOffset = m_bFDefaultOffSetEnabled ? m_fDefaultOffset : sf::Vector2f{};
 
 	RescaleSkeleton();
 	RescaleTime();
@@ -377,22 +503,22 @@ void CSfmlSpinePlayer::ResizeWindow()
 {
 	if (m_window.get() != nullptr)
 	{
-		m_window->setSize(sf::Vector2u(static_cast<unsigned int>(m_fMaxWidth * m_fSkeletonScale), static_cast<unsigned int>(m_fMaxHeight * m_fSkeletonScale)));
+		m_window->setSize(sf::Vector2u(static_cast<unsigned int>(m_fBaseSize.x * m_fSkeletonScale), static_cast<unsigned int>(m_fBaseSize.y * m_fSkeletonScale)));
 	}
 }
 /*視点移動*/
 void CSfmlSpinePlayer::MoveViewPoint(int iX, int iY)
 {
-	m_iOffset.x += iX;
-	m_iOffset.y += iY;
+	m_fOffset.x += iX;
+	m_fOffset.y += iY;
 	for (size_t i = 0; i < m_drawables.size(); ++i)
 	{
-		m_drawables.at(i)->skeleton->x = (m_fMaxWidth - m_iOffset.x) / 2;
-		m_drawables.at(i)->skeleton->y = (m_fMaxHeight - m_iOffset.y) / 2;
+		m_drawables.at(i)->skeleton->x = (m_fBaseSize.x - m_fOffset.x) / 2;
+		m_drawables.at(i)->skeleton->y = (m_fBaseSize.y - m_fOffset.y) / 2;
 	}
 }
-/*場面移行*/
-void CSfmlSpinePlayer::ShiftScene()
+/*動作移行*/
+void CSfmlSpinePlayer::ShiftAnimation()
 {
 	++m_nAnimationIndex;
 	if (m_nAnimationIndex > m_animationNames.size() - 1)m_nAnimationIndex = 0;
--- OtogiFrontierPlayer/sfml_spine_player.h
+++ OtogiFrontierPlayer/sfml_spine_player.h
@@ -22,14 +22,15 @@ private:
 
 	std::unique_ptr<sf::RenderWindow> m_window;
 
-	float m_fMaxWidth = 1920.f;
-	float m_fMaxHeight = 1080.f;
-	float m_fDefaultWindowScale = 1.f;
+	sf::Vector2f m_fBaseSize = sf::Vector2f{ 1920.f, 1080.f };
+	float m_fDefaultScale = 1.f;
 	float m_fThresholdScale = 1.f;
+	sf::Vector2f m_fDefaultOffset{};
+	bool m_bFDefaultOffSetEnabled= false;
 
 	float m_fTimeScale = 1.f;
 	float m_fSkeletonScale = 1.f;
-	sf::Vector2i m_iOffset{};
+	sf::Vector2f m_fOffset{};
 
 	std::vector<std::string> m_animationNames;
 	size_t m_nAnimationIndex = 0;
@@ -38,6 +39,7 @@ private:
 	size_t m_nAudioIndex = 0;
 
 	bool SetupDrawer();
+	void WorkOutDefualtSize();
 	void WorkOutDefaultScale();
 
 	void RescaleSkeleton();
@@ -46,7 +48,7 @@ private:
 	void ResizeWindow();
 
 	void MoveViewPoint(int iX, int iY);
-	void ShiftScene();
+	void ShiftAnimation();
 
 	void Redraw(float fDelta);
 };
